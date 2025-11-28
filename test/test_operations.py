17fb46c6490b6d908253842ef48c0c3d06ed201d
Bikram-jena <bikramjena899@gmail.com>
2025-11-28 23:17:10 +0530

    Unit test case updated

---
+++ test/test_operations.py
@@ -0,0 +1,9 @@
+from src.math_operations import add, sub
+
+def test_add():
+    assert add(2,3) == 5
+    assert add(-3,3) == 0
+
+def test_sub():
+    assert sub(2,3) == -1
+    assert add(3,3) == 0
